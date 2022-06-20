a16a21a828d8a6ec7799374d20126d49aff83e59
mayank-cse <72187020+mayank-cse@users.noreply.github.com>
2022-06-20 14:20:55 +0530

    webpage

---
+++ ASPNET-Portfolio/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="Home" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ASPNET_Portfolio.Default" %>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="container" runat="server">
+        <div class="col-md-8 col-md-offset-2 panel">
+            <div class="text-vertical-center">
+                <h1 class="logo">Mayank Gupta</h1>
+                <p>
+                    <br/>
+                </p>
+                <h4>
+                    Hi, it is great to meet you. 
+                    <br/>I am Mayank Gupta, a Junior Computer Programmer, <br/>and welcome to my portfolio website.
+                </h4>
+                <br/>
+                <a href="About.aspx" class="btn btn-dark btn-lg">About Me</a>
+            </div>
+        </div>
+        
+    </div>
+</asp:Content>
---
+++ ASPNET-Portfolio/Site.Master.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+/*
+File name: Site.Master.cs
+Author's name: Hae Yeon Kang (Lucy)
+web site name: Hae Yeon's Portfolio Website
+file description: C# file to control presentation of sites.
+*/
+
+namespace ASPNET_Portfolio
+{
+    public partial class Site : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            /**
+             * This will modify some of the control's visibility including navbar, footer and css files.
+             */
+            switch (Page.Title)
+            {
+                case "Home":
+                    full.Attributes.Add("class", "full");
+                    navbarHidden.Visible = false;
+                    footerHidden.Visible = false;
+                    appcss.Visible = false;
+                    homepagecss.Visible = true;
+                    break;
+                default:
+                    appcss.Visible = true;
+                    homepagecss.Visible = false;
+                    break;
+            }
+        }
+    }
+}
